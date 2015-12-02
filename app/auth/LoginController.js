922e9fb01faa88463e20a29d89fd0261433d4aac
Tomasz Guzialek <tomasz.guzialek@qiero.org>
2015-12-02 21:53:49 +0100

    Login form proof of concept.

--- app/auth/LoginController.js
+++ app/auth/LoginController.js
@@ -1,7 +1,37 @@
 (function() {
-    var auth = angular.module('auth', []);
+    var auth = angular.module('auth', ['pouchdb']);
     
-    auth.controller('LoginController', function() {
+    auth.config(function(pouchDBProvider, POUCHDB_METHODS) {
+        var authMethods = {
+            login: 'qify',
+            logout: 'qify'
+        };
+        pouchDBProvider.methods = angular.extend({}, POUCHDB_METHODS, authMethods);
     });
+    
+    auth.controller('LoginController', ['pouchDB', function(pouchDB) {
+        
+        this.username = null;
+        this.password = null;
+        
+        this.db = new PouchDB('http://localhost:5984/_users', {skipSetup: true});
+        
+        this.login = function() {
+            this.db.login(this.username, this.password, function (err, response) {
+                if (err) {
+                    if (err.name === 'unauthorized') {
+                        console.log("Unauthorized!");
+                    } else {
+                        console.log('Error during login: ' + err);
+                    }
+                    return;
+                }
+                
+                console.log('Logged in!');
+            });
+        };
+        
+    }]);
+            
 })();
 
