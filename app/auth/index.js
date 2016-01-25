7012952ebd6d4a6a7935b41ecee6a9e0ef943475
Tomasz Guzialek <tomasz.guzialek@qiero.org>
2016-01-26 00:51:30 +0100

    Require()-ing hoodie in the controller.

--- app/auth/index.js
+++ app/auth/index.js
@@ -1,7 +1,9 @@
 var angular = require('angular');
+var Hoodie = require('hoodie-client');
 
 module.exports = angular.module('auth', [])
     .controller('LoginController', function() {
         console.log("Entered LoginController");
+        this.hoodie = new Hoodie();
     }
 );
