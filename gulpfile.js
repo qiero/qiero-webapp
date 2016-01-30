9aa9526937ef9e4f1cd3f987fd954dfeb6102b6d
Tomasz Guzialek <tomasz.guzialek@qiero.org>
2016-01-30 12:21:06 +0100

    Build and clean tasks for Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 var gulp = require('gulp');
 var exec = require('child_process').exec;
+var del = require('del');
 
+/* Does not watch for changes in node_modules! */ 
 gulp.task('watch', function() {
     gulp.watch('./app/**/*.html', ['copy-static']);
     gulp.watch('./app/**/*.js', ['copy-static']);
     gulp.watch('./app/manifest.appcache', ['copy-static']);
 });
 
-gulp.task('hoodie-start', ['copy-static', 'copy-node-modules'],  function() {
+gulp.task('build', ['copy-static', 'copy-node-modules']);
+
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+gulp.task('hoodie-start', ['build'],  function() {
   var child = exec('node ./node_modules/hoodie-server/bin/start --www dist --custom-ports 6001,6002,6003');
   child.stdout.on('data', function(data) {
       console.log(data);
